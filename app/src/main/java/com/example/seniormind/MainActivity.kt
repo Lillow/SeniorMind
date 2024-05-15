c1f16ad7c306f4bdfba21dfcaba0135cb0555277
Danillo <danillordm@gmail.com>
2024-05-15 15:18:27 -0300

    commit inicial

---
+++ app/src/main/java/com/example/seniormind/MainActivity.kt
@@ -0,0 +1,15 @@
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
+    }
+
+}
